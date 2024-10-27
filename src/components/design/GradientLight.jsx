890df47d8b48669ec3ea71a544b44e1e62857c21
MitanshuPatel999 <MitanshuPatel999@users.noreply.github.com>
2024-10-27 05:57:32 +0000

    deploy: 0edce3256ee4f0aa9da80072c0ba281a63c2a957

--- src/components/design/GradientLight.jsx
+++
@@ -1,5 +0,0 @@
-export const GradientLight = () => {
-  return (
-    <div className="absolute top-0 left-0 w-[120%] aspect-square bg-radial-gradient from-[#38b6ff91] to-[#FF3131]/0 to-70% pointer-events-none opacity-75" />
-  );
-};
