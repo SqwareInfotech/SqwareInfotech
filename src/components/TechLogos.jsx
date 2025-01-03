0baeea4389d267dd55847f1bd9cb3cb3c56d2faf
MitanshuPatel999 <mitanshu.999patel@gmail.com>
2025-01-04 00:15:37 +0530

    improvement: change logo SVGs and display of infinite technology slider for mobile

--- src/components/TechLogos.jsx
+++ src/components/TechLogos.jsx
@@ -7,10 +7,10 @@ const TechLogos = ({ className }) => {
       <h5 className="tagline my-3 text-center text-n-1/50">
         Powering Solutions With Leading Technologies
       </h5>
-      <Marquee pauseOnHover="true" className="[mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+      <Marquee pauseOnHover="true" className="[mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-100px),transparent_100%)] md:[mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
         {techLogos.map((logo, index) => (
           <div
-            className="flex items-center justify-centerc mx-10 flex-1 h-[8.5rem]"
+            className="flex items-center justify-centerc mx-8 md:mx-10 flex-1 h-[8.5rem]"
             key={index}
           >
             <img src={logo.src} width={logo.width? logo.width : undefined} height={logo.height? logo.height : undefined} alt={logo} />
